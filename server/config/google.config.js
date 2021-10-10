bfe0bc02e949aa95a3e9f5cf6ed9a4388b6c6b43
Saam <saamjwesley@gmail.com>
2021-10-11 00:24:22 +0530

    google auth

---
+++ server/config/google.config.js
@@ -0,0 +1,36 @@
+import passport from "passport";
+import googleOAuth from "passport-google-oauth20";
+
+import { UserModel } from "../database/allModels";
+
+const GoogleStrategy = googleOAuth.Strategy;
+
+export default (passport) => {
+    passport.use(
+        new GoogleStrategy({
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: "http://localhost:400/auth/google/callback"
+        },
+            async (accessToken, refreshToken, profile, done) => {
+                const newUser = {
+                    fullname: profile.displayName,
+                    email: profile.emails[0].value,
+                    ProfilePic: profile.photos[0].value
+                };
+                try {
+                    const user = await UserModel.findOne({ email: newUser.email });
+                    const token = user.generateJwtToken();
+                    if (user) {
+                        document(null, token);
+                    } else {
+                        const user = await UserModel.create(newUser);
+                    }
+                } catch (error) {
+                    done(error, null);
+                }
+            }
+
+        )
+    )
+}
